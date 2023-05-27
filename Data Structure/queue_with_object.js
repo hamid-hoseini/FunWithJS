bc475e685c41b110680ef01da6c324cd49c3a8fb
Hamid <mail.hamid.hoseini@gmail.com>
2023-05-26 22:04:58 -0700

    Queue circular

--- Data Structure/queue_with_object.js
+++ Data Structure/queue_with_object.js
@@ -1,43 +1,48 @@
 class Queue {
   constructor() {
-    this.items = [];
+    this.items = {};
+    this.front = 0;
+    this.rear = 0;
   }
 
   enqueue(element) {
-    this.items.push(element);
+    this.items[this.rear] = element;
+    this.rear++;
   }
 
   dequeue() {
-    return this.items.shift();
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
   }
 
   peek() {
-    if (!this.isEmpty()) {
-      return this.items[0];
-    }
-    return null;
-  }
-
-  isEmpty() {
-    return this.items.length === 0;
+    return this.items[this.front];
   }
 
   size() {
-    return this.items.length;
+    return this.rear - this.front;
+  }
+
+  isEmpty() {
+    return this.rear - this.front === 0;
   }
 
   print() {
-    console.log(this.items.toString());
+    console.log(this.items);
   }
 }
 
 const queue = new Queue();
 console.log(queue.isEmpty());
-queue.enqueue(10);
-queue.enqueue(20);
-queue.enqueue(30);
+queue.enqueue(42);
+queue.enqueue(27);
+queue.enqueue(39);
+queue.enqueue(53);
 console.log(queue.size());
 queue.print();
 console.log(queue.dequeue());
 console.log(queue.peek());
+console.log(queue.isEmpty());
 queue.print();
