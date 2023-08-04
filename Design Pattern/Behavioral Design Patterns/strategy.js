3c0b124c3baec362cb7766c48198d69008aabfee
Hamid <mail.hamid.hoseini@gmail.com>
2023-08-03 22:49:57 -0700

    Usage of Strategy design pattern

--- Design Pattern/Behavioral Design Patterns/strategy.js
+++ Design Pattern/Behavioral Design Patterns/strategy.js
@@ -11,3 +11,14 @@ const quickSort = dataset => {
   // ...
   return dataset
 }
+
+// And it can be used as
+
+const longDataSet = [1, 5, 4, 3, 2, 8]
+const shortDataSet = [1, 5, 4]
+
+const sorter1 = sorter(longDataSet)
+const sorter2 = sorter(shortDataSet)
+
+sorter1(longDataSet) // Output : Sorting with quick sort
+sorter2(shortDataSet) // Output : Sorting with bubble sort
