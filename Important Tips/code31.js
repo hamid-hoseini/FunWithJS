f8e1b2d53b11781f4d20ffe5343a92eb0e2a9e15
Hamid <mail.hamid.hoseini@gmail.com>
2023-10-05 22:15:17 -0700

    the easiest way to resize an array

---
+++ Important Tips/code31.js
@@ -0,0 +1,18 @@
+// the easiest way to resize an array
+
+// The length property of an array is useful to resize or empty an array quickly. 
+// Let's apply length property on number array to resize the number of elements from 5 to 2,
+
+var array = [1, 2, 3, 4, 5];
+console.log(array.length); // 5
+
+array.length = 2;
+console.log(array.length); // 2
+console.log(array); // [1,2]
+
+// and the array can be emptied too
+
+var array = [1, 2, 3, 4, 5];
+array.length = 0;
+console.log(array.length); // 0
+console.log(array); // []
