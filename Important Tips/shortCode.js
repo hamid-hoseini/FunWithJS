ef70eca24971ddacad76275d494f16cc817d08b4
Hamid <mail.hamid.hoseini@gmail.com>
2023-11-17 21:10:09 -0800

    add getters and setters using defineProperty method

--- Important Tips/shortCode.js
+++ Important Tips/shortCode.js
@@ -660,3 +660,26 @@ var b = +a;
 console.log(typeof a, typeof b, b); // string, number, NaN
 
 
+// 50. perform language specific date and time formatting
+
+/** You can use the Intl.DateTimeFormat object which is a constructor for objects that enable 
+ * language-sensitive date and time formatting. Let's see this behavior with an example,
+ */ 
+
+var date = new Date(Date.UTC(2019, 7, 7, 3, 0, 0));
+console.log(new Intl.DateTimeFormat("en-GB").format(date)); // 07/08/2019
+console.log(new Intl.DateTimeFormat("en-AU").format(date)); // 07/08/2019
+
+
+// 51. different error names from error object
+// There are 6 different types of error names returned from error object,
+
+/**
+EvalError: An error has occurred in the eval() function
+RangeError:	An error has occurred with a number "out of range"
+ReferenceError:	An error due to an illegal reference
+SyntaxError: An error due to a syntax error
+TypeError: An error due to a type error
+URIError: An error due to encodeURI()
+*/
+
