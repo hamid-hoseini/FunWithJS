af8e4a5eba6538ea473f342ea5ce8cdca5a80bc9
Hamid <mail.hamid.hoseini@gmail.com>
2023-12-29 22:23:04 -0800

    singly linked list insert/remove/reverse

--- Data Structure/linked_list_tail1.js
+++ Data Structure/linked_list_tail1.js
@@ -88,5 +88,44 @@ class SinglyLinkedList{
       }
       return false
   }
+  // The insert method takes an index number and a value as parameters, and inserts the value at the given index in the list
+  insert(index, val) {
+      if (index < 0 || index > this.length) return false
+      if (index === this.length) return !!this.push(val)
+      if (index === 0) return !!this.unshift(val)
 
+      const newNode = new Node(val)
+      const prev = this.get(index - 1)
+      const temp = prev.next
+      prev.next = newNode
+      newNode.next = temp
+      this.length++
+      return true
+  }
+  // The remove method takes an index number as parameter and removes the node at the given index in the list
+  remove(index) {
+      if(index < 0 || index >= this.length) return undefined
+      if(index === 0) return this.shift()
+      if(index === this.length - 1) return this.pop()
+      const previousNode = this.get(index - 1)
+      const removed = previousNode.next
+      previousNode.next = removed.next
+      this.length--
+      return removed
+  }
+  // The reverse method reverses the list and all pointers so that the head becomes the tail and the tail becomes the head
+  reverse(){
+    const node = this.head
+    this.head = this.tail
+    this.tail = node
+    let next
+    const prev = null
+    for(let i = 0; i < this.length; i++) {
+      next = node.next
+      node.next = prev
+      prev = node
+      node = next
+    }
+    return this
+  }
 }
