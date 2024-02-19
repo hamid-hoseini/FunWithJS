78aa2db3d5f263ebde6963873c98fdf6016c05ae
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2024-02-19 15:00:14 -0800

    Kadane's Algorithm

--- Interview Questions/KadaneAlgorithm.js
+++ Interview Questions/KadaneAlgorithm.js
@@ -20,3 +20,19 @@ function kadane(arr) {
 
   return maxSoFar;
 }
+
+/*
+Kadane's Algorithm is an efficient algorithm for solving the maximum subarray sum problem. 
+It is named after its inventor, Jay Kadane, and is used to find the contiguous subarray with the largest sum within a given array of numbers.
+The algorithm works by iterating through the array and keeping track of the maximum sum encountered so far and the current sum of the subarray 
+being considered. It starts with the assumption that the maximum sum is the first element of the array and updates the current sum by either 
+adding the next element or starting a new subarray if the current sum becomes negative.
+
+Here is a step-by-step explanation of Kadane's Algorithm:
+
+1- Initialize two variables, maxSoFar, and maxEndingHere, to the value of the first element in the array.
+2- Iterate through the array starting from the second element:
+    - Update maxEndingHere by taking the maximum value between the current element and the sum of the current element and maxEndingHere.
+    - Update maxSoFar by taking the maximum value between maxSoFar and maxEndingHere.
+3- After the iteration, maxSoFar will contain the maximum sum found in any subarray of the given array.
+*/
