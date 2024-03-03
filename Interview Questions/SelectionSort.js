d579f1df25a13c0f7aad88d9cf1e8a881c8bb863
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2024-03-02 22:42:10 -0800

    Google coding interview question Selection Sort

--- Interview Questions/SelectionSort.js
+++ Interview Questions/SelectionSort.js
@@ -31,4 +31,14 @@ const numbers = [64, 25, 12, 22, 11];
 console.log("Original array:", numbers);
 console.log("Sorted array:", selectionSort(numbers));
 
+/*
+Selection sort is a simple sorting algorithm that works by repeatedly finding the minimum element 
+from the unsorted part of the array and placing it at the beginning.
+the selectionSort function takes an array arr as input and performs selection sort on it. 
+It iterates through the array starting from the first element and finds the index of the 
+minimum element in the remaining unsorted part of the array. Once the minimum element is found, 
+it is swapped with the first element in the unsorted part. This process continues until the entire array is sorted.
 
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
