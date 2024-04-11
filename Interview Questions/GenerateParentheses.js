c5fba253923795175185256c608595b8ba24f626
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2024-04-10 23:20:24 -0700

    Solution 2 Using DFS for Generate Parentheses

--- Interview Questions/GenerateParentheses.js
+++ Interview Questions/GenerateParentheses.js
@@ -73,3 +73,53 @@ Output
 
 // Time complexity: O(2^n), as there are 2^n possible combinations of ‘(‘ and ‘)’ parentheses.
 // Auxiliary space: O(n), as n characters are stored in the str array.
+
+
+// Solution 2 Using DFS
+
+/*
+Create two variables left and right representing the number of remaining open parentheses and number of remaining close parentheses.
+During each recursive call, two possibilities are explored:
+Adding an open parenthesis ‘(‘ to the current combination and decrementing left.
+Adding a close parenthesis ‘)‘ to the current combination and decrementing right.
+If at any point, the number of open parentheses becomes greater than the number of close 
+parentheses or either left or right becomes negative, the combination is invalid, and that branch of the recursion is terminated.
+When both left and right are zero, it will indicate a valid combination has been formed.
+*/
+
+function generateParenthesis(left, right, s, answer) {
+	// terminate
+	if (left == 0 && right == 0) {
+	answer.push(s);
+	}
+
+	if (left > right || left < 0 || right < 0) {
+	// wrong
+	return;
+	}
+
+	s += '{';
+	generateParenthesis(left - 1, right, s, answer);
+	s = s.slice(0, -1);
+
+	s += '}';
+	generateParenthesis(left, right - 1, s, answer);
+	s = s.slice(0, -1);
+}
+
+function main() {
+	let n = 3;
+	// array ans is created to store all the possible valid
+	// combinations of the parentheses.
+	let ans = [];
+	let s = '';
+	// initially we are passing the counts of open and close
+	// as 0, and the string s as an empty string.
+	generateParenthesis(n, n, s, ans);
+	// Now, here we print out all the combinations.
+	for (let k of ans) {
+	console.log(k);
+	}
+}
+
+main();
