bd6706640dc2c9e6630c487bc0288baeaddabdcc
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2024-05-01 23:28:26 -0700

    Coding Inteview Question - LongestvalidParentheses

---
+++ Interview Questions/LongestvalidParentheses.js
@@ -0,0 +1,19 @@
+// Coding Inteview Question
+
+/*
+// https://www.geeksforgeeks.org/length-of-the-longest-valid-substring/
+Given a string consisting of opening and closing parenthesis, find the length of the longest valid parenthesis substring.
+
+Examples: 
+
+Input: ((()
+Output : 2
+Explanation : ()
+Input: )()())
+Output : 4
+Explanation: ()() 
+Input:  ()(()))))
+Output: 6
+Explanation:  ()(())
+
+*/
