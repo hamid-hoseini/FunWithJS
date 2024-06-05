6efb47ee62c13df3e525a7d8f906c8561d0241cf
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2024-06-05 00:08:06 -0700

    Coding Interview Questions Permutations

--- Interview Questions/Permutations.js
+++ Interview Questions/Permutations.js
@@ -81,3 +81,67 @@ for (let i = 0;
     i < uniquePermutations.length; i++) {
     console.log(uniquePermutations[i]);
 }
+
+
+
+// solution 3: Using Heap’s Algorithm
+
+function swap(strArr, i, j) {
+  const temp = strArr[i];
+  strArr[i] = strArr[j];
+  strArr[j] = temp;
+}
+
+function permute(str, n = str.length, strArr = str.split('')) {
+  if (n === 1) {
+    console.log(strArr.join(''));
+  } else {
+    for (let i = 0; i < n; i++) {
+      permute(str, n - 1, strArr);
+      if (n % 2 === 0) {
+        swap(strArr, i, n - 1);
+      } else {
+        swap(strArr, 0, n - 1);
+      }
+    }
+  }
+}
+
+const str = 'abcd';
+permute(str);
+
+
+
+// solution 4: Using Iterative Method with Factorial Count
+
+function generatePermutationsWithFactorial(str) {
+    const permutations = [];
+    const factorial = n => n <= 1 ? 1 : n * factorial(n - 1);
+
+    const len = str.length;
+    const totalPermutations = factorial(len);
+
+    for (let i = 0; i < totalPermutations; i++) {
+        let currentPermutation = '';
+        let temp = i;
+
+        const availableChars = str.split('');
+
+        for (let j = len - 1; j >= 0; j--) {
+            const index = temp % (j + 1);
+            temp = Math.floor(temp / (j + 1));
+
+            currentPermutation += availableChars[index];
+            availableChars.splice(index, 1);
+        }
+
+        permutations.push(currentPermutation);
+    }
+
+    return permutations;
+}
+
+const str = "ABC";
+const permutations = generatePermutationsWithFactorial(str);
+console.log("Permutations:");
+console.log(...permutations);
