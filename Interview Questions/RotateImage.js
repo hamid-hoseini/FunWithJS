7079e37f05226a14705440497eda5da04a8765a4
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2024-06-12 23:20:12 -0700

    Coding Interview Question RotateImage

--- Interview Questions/RotateImage.js
+++ Interview Questions/RotateImage.js
@@ -1,6 +1,7 @@
 // Coding Interview Question
 
 /*
+Reference: https://www.geeksforgeeks.org/rotate-image-by-90-degree-using-javascript/
 You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
 
 You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. 
@@ -52,3 +53,45 @@ The matrix obtained is rotated matrix (by 90 degrees).
 
 // Time Complexity: O(N*N) , transposing the matrix + O(N*N) , reversing rows = O(N*N)
 // Space Complexity: O(1), constant space
+
+
+//  Solution 2:
+
+function rotate(matrix) {
+    let n = matrix.length;
+    
+    let temp = new Array(n).fill().map(() => new Array(n).fill(0));
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            temp[j][n - i - 1] = matrix[i][j];
+        }
+    }
+    return temp;
+}
+
+function main() {
+    let arr = [
+        [1, 2, 3 ],
+        [4, 5, 6 ],
+        [7 , 8, 9]
+    ];
+    let answer = rotate(arr);
+    console.log("Rotated image by 90 degree in clockwise direction ");
+    for (let i = 0; i < answer.length; i++) {
+        for (let j = 0; j < answer[0].length; j++) {
+            process.stdout.write(answer[i][j] + " ");
+        }
+        console.log();
+    }
+}
+
+// Time Complexity: O(N*N) , we traverse original matrix.
+// Space Complexity: O(N*N) , created matrix of same size.
+
+
+/*
+We will create another temporary matrix of same size of original matrix i.e. n*n and then we take 
+the first row of original matrix and put it in the last column of the temporary matrix , take second row of 
+original matrix and put it in the second last column of matrix , take third row of original matrix and put it in the
+third last column of matrix and so.
+*/
