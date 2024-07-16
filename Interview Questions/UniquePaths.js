b65c1dc90b7f50d92447849bf9f01fbd6d0a4060
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2024-07-15 23:57:56 -0700

    Coding Interview Question UniquePaths

--- Interview Questions/UniquePaths.js
+++ Interview Questions/UniquePaths.js
@@ -92,4 +92,48 @@ function numberOfPaths(n, m, DP){
  
 document.write(numberOfPaths(3, 3,DP));
 
+// Time Complexity: O(N * M)
+// Auxiliary Space: (N * M)
+
+
+// Solution 3: Dynamic Programming(DP)
+
+
+// Returns count of possible paths to reach
+// cell at row number m and column number n from
+// the topmost leftmost cell (cell at 1, 1)
+function numberOfPaths(m , n)
+{
+    // Create a 2D table to store results
+    // of subproblems
+    var count = Array(m).fill(0).map(x => Array(n).fill(0));
+ 
+    // Count of paths to reach any cell in
+    // first column is 1
+    for (i = 0; i < m; i++)
+        count[i][0] = 1;
+ 
+    // Count of paths to reach any cell in
+    // first row is 1
+    for (j = 0; j < n; j++)
+        count[0][j] = 1;
+ 
+    // Calculate count of paths for other
+    // cells in bottom-up manner using
+    // the recursive solution
+    for (i = 1; i < m; i++) {
+        for (j = 1; j < n; j++)
+ 
+            // By uncommenting the last part the
+            // code calculates the total possible paths
+            // if the diagonal Movements are allowed
+             
+            //+ count[i-1][j-1];
+            count[i][j] = count[i - 1][j] + count[i][j - 1]; 
+    }
+    return count[m - 1][n - 1];
+}
+ 
+// Driver program to test above function
+document.write(numberOfPaths(3, 3));
 
