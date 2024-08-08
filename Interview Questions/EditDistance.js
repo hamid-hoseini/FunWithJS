e21e279771f1d23a1624e2b4e635d9eaf2e12789
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2024-08-07 23:52:21 -0700

    Coding Interview Question EditDistance

--- Interview Questions/EditDistance.js
+++ Interview Questions/EditDistance.js
@@ -31,3 +31,56 @@ exention -> exection (replace 'n' with 'c')
 exection -> execution (insert 'u')
 
 */
+
+// Javascript program to
+// find minimum numberoperations
+// to convert str1 to str2
+
+function editDist(str1, str2, m, n)
+{
+    // If first string is empty, the 
+    // only option is to insert all 
+    // characters of second string into first
+    if (m == 0)
+        return n;
+
+    // If second string is empty, the only
+    // option is to remove all characters 
+    // of first string
+    if (n == 0)
+        return m;
+
+    // If last characters of two strings are
+    // same, nothing much to do. Get the count for remaining 
+    // strings.
+    if (str1[m - 1] == str2[n - 1])
+        return editDist(str1, str2, m - 1, n - 1);
+
+    // If last characters are not same, consider all
+    // three operations on last character of first
+    // string, recursively compute minimum cost for all
+    // three operations and take minimum of three
+    // values.
+    return 1 + 
+    min(editDist(str1, str2, m, n - 1), // Insert
+        editDist(str1, str2, m - 1, n), // Remove
+        editDist(str1, str2, m - 1, n - 1)); // Replace
+}
+
+function min(x, y, z)
+{
+   return Math.min(x, Math.min(y, z));
+}
+
+// Driver code
+let str1 = "GEEXSFRGEEKKS";
+let str2 = "GEEKSFORGEEKS";
+console.log(editDist(str1, str2, str1.length, 
+                                    str2.length));
+
+/*
+Time Complexity: O(3^m), when length of “str1” >= length of “str2” and O(3^n), when length of “str2” > length of “str1”. Here m=length of “str1 and n=length of “str2”
+Auxiliary Space: O(m), when length of “str1” >= length of “str2” and O(n), when length of “str2” > length of “str1”. Here m=length of “str1 and n=length of “str2”
+*/
+
+
