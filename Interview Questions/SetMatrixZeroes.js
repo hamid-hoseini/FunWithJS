d31c7f375de010cb0e20f5bab04a5f1fa981016f
Hamid Hoseini <mail.hamid.hoseini@gmail.com>
2024-08-16 23:29:26 -0700

    Coding Interview Question SetMatrixZeroes

--- Interview Questions/SetMatrixZeroes.js
+++ Interview Questions/SetMatrixZeroes.js
@@ -73,3 +73,53 @@ for (let i = 0; i < arr.length; i++) {
 // Time Complexity: O((N*M)*(N + M)) + O(N*M), where N = no. of rows in the matrix and M = no. of 
 // columns in the matrix. we are traversing the matrix to find the cells with the value 0. It takes O(N*M).
 // Auxillary space: O(1) as we are not using any extra space.
+
+
+// Better Approach:
+
+function setMatrixZeroes(matrix) {
+    const n = matrix.length;
+    const m = matrix[0].length;
+
+    // To store which rows and columns are supposed to be marked with zeroes.
+    const row = new Array(n).fill(0);
+    const col = new Array(m).fill(0);
+
+    // Traverse the matrix using nested loops
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            // If the cell contains zero, mark its row and column as zero
+            if (matrix[i][j] === 0) {
+                row[i] = 1;
+                col[j] = 1;
+            }
+        }
+    }
+
+    for (let i = 0; i < n; i++) {
+        let rowString = "";
+        for (let j = 0; j < m; j++) {
+            // Set cells to zero if any of the row[i] or col[j] is 1
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+            rowString += matrix[i][j] + " ";
+        }
+        console.log(rowString);
+    }
+}
+
+// Driver Code
+const arr = [
+    [0, 1, 2, 0],
+    [3, 4, 5, 2],
+    [1, 3, 1, 5]
+];
+
+// Function call
+setMatrixZeroes(arr);
+
+
+// Time Complexity: O(2*(N*M)), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+// Auxillary Space: O(N) + O(M), O(N) is for using the row array and O(M) is for using the col array.
+
